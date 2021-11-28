fe0ca36cc0c1e841966fb08003e927aa911535c7
Ashish6063 <75639559+Ashish6063@users.noreply.github.com>
2021-11-29 04:06:55 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+function clr(){
+document.getElementById("screen").value = " "
+}
+
+function display(val){
+    document.getElementById("screen").value+=val
+}
+
+function equate(){
+    let x = document.getElementById("screen").value
+    let y = eval(x)
+    document.getElementById("screen").value = y
+}
